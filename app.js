39d722347282c59326463715364c6b2800b91218
Newton <kamaunewton78@gmail.com>
2020-07-08 12:07:38 +0300

    parse the city name to our query instead of using hard coded city name

--- app.js
+++ app.js
@@ -7,43 +7,42 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 app.post("/", function (req, res) {
-   console.log(req.body.cityName);
-    
+
+  var query = req.body.cityName;
+  const apiKey = "9eb2abc96b224e0a1904e7b41250a153";
+  const units = "metrics";
+  var url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      query +
+      "&appid=" +
+      apiKey +
+      "&units=" +
+      units;
+
+  https.get(url, function (response) {
+      console.log(response.statusCode);
+
+      response.on("data", function (data) {
+          const weatherData = JSON.parse(data);
+          // console.log(weatherData);
+          const temp = weatherData.main.temp;
+          console.log(temp);
+          const weatherDescription = weatherData.weather[0].description;
+          console.log(weatherDescription);
+          const weatherIcon = weatherData.weather[0].icon;
+          const imageUrl =
+              "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+          res.write("<p>The clouds are " + weatherDescription + "</p>");
+          res.write("<img src=" + imageUrl + ">");
+          res.write(
+              "<h1>The temperature in "+ query+" is " + temp + "degress celsius</h1>"
+          );
+          res.send();
+      });
+  });
 });
-// var query = "Nairobi";
-// const apiKey = "9eb2abc96b224e0a1904e7b41250a153";
-// const units = "metrics";
-// var url =
-//     "https://api.openweathermap.org/data/2.5/weather?q=" +
-//     query +
-//     "&appid=" +
-//     apiKey +
-//     "&units=" +
-//     units;
-
-// https.get(url, function (response) {
-//     console.log(response.statusCode);
-
-//     response.on("data", function (data) {
-//         const weatherData = JSON.parse(data);
-//         // console.log(weatherData);
-//         const temp = weatherData.main.temp;
-//         console.log(temp);
-//         const weatherDescription = weatherData.weather[0].description;
-//         console.log(weatherDescription);
-//         const weatherIcon = weatherData.weather[0].icon;
-//         const imageUrl =
-//             "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-//         res.write("<p>The clouds are " + weatherDescription + "</p>");
-//         res.write("<img src=" + imageUrl + ">");
-//         res.write(
-//             "<h1>The temperature in Nairobi is " + temp + "degress celsius</h1>"
-//         );
-//         res.send();
-//     });
-// });
 
 app.listen(3000, () => console.log("serving art 300"));
